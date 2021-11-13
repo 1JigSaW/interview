b02511bc068a6fc9a7bded90bbf08b71f15970d3
JigSaW <misho_1998@mail.ru>
2021-11-14 00:58:59 +0300

    first proj

---
+++ open_close_file.py
@@ -0,0 +1,21 @@
+# from contextlib import contextmanager
+
+# @contextmanager
+# def processor():
+# 	print('--> start processing')
+# 	yield
+# 	print('<-- stop processing')
+
+# with processor():
+# 	print('::processing')
+
+from contextlib import contextmanager
+
+@contextmanager
+def open_file(path, mode):
+	f = open(path, mode)
+	yield f
+	f.close()
+
+with open_file('test.txt', 'w') as fw:
+	fw.write('hello')
